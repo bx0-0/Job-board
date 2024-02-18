42f0f09805c5ac7af8a981e365c7a719b8f54651
bx0-0 <149119238+bx0-0@users.noreply.github.com>
2024-02-19 00:09:27 +0200

    Update form.py
    Update form.py

--- job/form.py
+++ job/form.py
@@ -6,7 +6,7 @@ from django.db import transaction
 @transaction.atomic
 class ApplyForm(forms.ModelForm):
     cv = forms.FileField(disabled=True, label='Please note that cv will  automatically be uploaded ',required=False)
-
+    url = forms.URLField(required=False,help_text='URL to live site if  available or url to  your github account')
     class Meta:
         model = Apply
         fields = ["name", "mail", "url", "info_for_applier"]
